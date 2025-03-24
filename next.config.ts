9ec87ca67dea470f92e9e17bcca8a21edc9f6e34
Zobex <zobex.game@gmail.com>
2025-03-24 14:25:57 +0500

    add path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  basePath: "/myPo-byRai",
+  assetPrefix: "/myPo-byRai",
 };
 
 export default nextConfig;
